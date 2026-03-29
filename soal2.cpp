cd63d8cec1d877a7f15836fe01893e62108c049b
caca9090 <alwayscoffe2@gmail.com>
2026-03-29 23:29:32 +0700

    Add files via upload

---
+++ soal2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct DataKendaraan {
+    string plat_nomor;
+    string jenis;
+    string nama_pemilik;
+    string alamat;
+    string kota;
+};
+
+int main() {
+    DataKendaraan mobil;
+    mobil.plat_nomor = "DA 4752 AL";
+    mobil.jenis = "PCX";
+    mobil.nama_pemilik = "Siti Nurchalisah";
+    mobil.alamat = "Jl.Ukhwah Islamiah";
+    mobil.kota = "Banjarmasin";
+
+    cout << "a. Plat Nomor Kendaraan : " << mobil.plat_nomor << endl;
+    cout << "b. Jenis Kendaraan      : " << mobil.jenis << endl;
+    cout << "c. Nama Pemilik         : " << mobil.nama_pemilik << endl;
+    cout << "d. Alamat               : " << mobil.alamat << endl;
+    cout << "e. Kota                 : " << mobil.kota << endl;
+
+    return 0;
+}
